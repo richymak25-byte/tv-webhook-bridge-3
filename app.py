feb5c5b2ed17614e18f1b6778d9d1dbe6c4dcc0f
richymak25-byte <richymak25@gmail.com>
2026-02-26 13:32:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def health():
     return "OK", 200    
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT"))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
